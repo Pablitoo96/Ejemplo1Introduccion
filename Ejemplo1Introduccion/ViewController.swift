216e873638d1809f21aff90e094b55c1cc1aebc3
pablomaruottolo <pablogabriel.maruottolo@cieep.com>
2023-09-28 13:52:23 +0200

    parte contador click terminada

--- Ejemplo1Introduccion/ViewController.swift
+++ Ejemplo1Introduccion/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var contador: UInt!
+    
+    @IBOutlet weak var lbResultado: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        contador = 0
     }
 
 
+    @IBAction func subirContador(_ sender: Any) {
+        
+        contador += 1
+        lbResultado.text = "\(String (contador)) Veces"
+    }
 }
 
